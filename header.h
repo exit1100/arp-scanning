284c61e189c4c478dd8e5b89bfe3baceaa6df695
yejun <exit3015@gmail.com>
2023-10-05 04:26:19 +0900

    [YEJUN] add : arp request sending

--- header.h
+++ header.h
@@ -6,15 +6,15 @@
 struct ethernet_header{
     uint8_t dhost[6] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
     uint8_t shost[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint16_t type = 0x0806;
+    uint16_t type = 0x0608; // little endian
 } __attribute__ ((__packed__));
 
 struct arp_header{
-    uint16_t h_type = 0x1;
-    uint16_t p_type = 0x0800;
+    uint16_t h_type = 0x0100;
+    uint16_t p_type = 0x0008; // little endian
     uint8_t h_size = 6;
     uint8_t p_size = 4;
-    uint16_t op = 1;
+    uint16_t op = 0x0100;
     uint8_t sha[6] = {0,}; //my mac
     uint8_t sip[4] = {0,}; //my IP
     uint8_t dha[6] = {0,}; //00:00:00:00:00:00
@@ -25,7 +25,7 @@ struct arp_header{
 struct ARP_Packet{
     struct ethernet_header ether;
     struct arp_header arp;
-    uint8_t padding[18] = {0,};
+    //uint8_t padding[18] = {0,};
 } __attribute__ ((__packed__));
 
 short dump_ethernet_header(struct ethernet_header *ethernet_header);
--- main.cpp
+++ main.cpp
@@ -10,15 +10,16 @@
 
 #define ETH_HEADER_OFFSET 14;
 
+uint8_t arpTable[256][6];
+uint8_t myMac[6];
+uint8_t myIp[4];
+
 void usage() {
     printf("syntax: arp-test <interface>\n");
     printf("sample: arp-test ens33\n");
 }
 
 void myinfo(){
-    char mac_address[18];
-    char ip_address[20]; // IP 주소를 저장할 문자열 배열
-
     FILE *fp;
     char buffer[80];
     fp = popen("ifconfig | grep -o -E '([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})'", "r");
@@ -26,20 +27,21 @@ void myinfo(){
         perror("popen");
         exit(1);
     }
-    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-        strncpy(mac_address, buffer, sizeof(mac_address));
+    if (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        sscanf(buffer, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &myMac[0], &myMac[1], &myMac[2], &myMac[3], &myMac[4], &myMac[5]);
     }
+    pclose(fp);
 
     fp = popen("hostname -I", "r");
     if (fp == NULL) {
         perror("popen");
         exit(1);
     }
-    if (fgets(buffer, sizeof(buffer), fp) != NULL) strncpy(ip_address, buffer, sizeof(ip_address));
+    if (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        sscanf(buffer, "%hhu.%hhu.%hhu.%hhu", &myIp[0], &myIp[1], &myIp[2], &myIp[3]);
+    }
     pclose(fp);
 
-    printf("MAC 주소: %s\n", mac_address);
-    printf("IP 주소: %s\n", ip_address);
 
 }
 
@@ -51,15 +53,29 @@ void *sendPacket(void *dev){
         fprintf(stderr, "pcap_open_live(%s) return null - %s\n", (char *)dev, errbuf);
         exit(0);
     }
+    uint8_t targetIp = 0;
+    struct ARP_Packet arp_packet;
+
+
+    memcpy(arp_packet.ether.shost, &myMac, 6);
+    memcpy(arp_packet.arp.sha, &myMac, 6);
+    memcpy(arp_packet.arp.sip, &myIp, 4);
+    //for(int i=0; i<6; i++) arp_packet.ether.shost[i] = myMac[i];
+    //for(int i=0; i<6; i++) arp_packet.ether.sha[i] = myMac[i];
+
+    //arp_packet.arp.sip = {myIp[0], myIp[1], myIp[2], myIp[3]};
 
     while (1) {
-        struct ARP_Packet arp_packet;
+        memcpy(arp_packet.arp.dip, &myIp, 3);
+        arp_packet.arp.dip[3] = targetIp;
         if (pcap_sendpacket(pcap2, (unsigned char*)&arp_packet, sizeof(arp_packet)) != 0){
             printf("Fail sendpacket 1\n");
             exit (-1);
         }
-        printf("\n\n");
-        usleep(1000);
+        printf("dst IP : %d.%d.%d.%d\n", arp_packet.arp.dip[0], arp_packet.arp.dip[01], arp_packet.arp.dip[2], arp_packet.arp.dip[3]);
+        if(targetIp == 255) sleep(10);
+        targetIp++;
+        usleep(3000);
     }
     pcap_close(pcap2);
     printf("Thread 2 Die!!\n");
@@ -71,14 +87,18 @@ int main(int argc, char* argv[]) {
         usage();
         return 0;
     }
+
     char * dev = argv[1];
     char errbuf[PCAP_ERRBUF_SIZE];
-
     struct ethernet_header *etherPointer;
     struct arp_header *arpPointer;
 
-    //pthread_t thread;
-    //pthread_create(&thread, 0, sendPacket, (void *) dev);
+    myinfo();
+    //printf("my MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", myMac[0], myMac[1], myMac[2], myMac[3], myMac[4], myMac[5]);
+    //printf("my IP : %d.%d.%d.%d\n", myIp[0], myIp[1], myIp[2], myIp[3]);
+
+    pthread_t thread;
+    pthread_create(&thread, 0, sendPacket, (void *) dev);
 
     pcap_t* pcap = pcap_open_live(dev , BUFSIZ, 1, 1000, errbuf);
     if (pcap == NULL) {
@@ -90,7 +110,6 @@ int main(int argc, char* argv[]) {
         struct pcap_pkthdr* header;
         const u_char* packet;
         uint16_t parsingType = 0;
-        int arpTable[256][6];
 
         int res = pcap_next_ex(pcap, &header, &packet);
         if (res == 0) continue;
